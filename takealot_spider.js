084949baf65dffa38aaa96e8a5308988013bc436
Abraham de Villiers <jaco_ftw@iafrica.com>
2016-09-18 16:54:17 +0200

    Takealot Spider. Scrape www.takealot.com/deals and www.takealot.com/app-deals Page. Record products name (title) in loop-array and writes to JSON file.

---
+++ takealot_spider.js
@@ -0,0 +1,41 @@
+var casper = require('casper').create(
+{
+	pageSettings: 
+	{
+		userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1588.0 Safari/537.36',
+	}
+});
+
+var fs = require('fs');
+var date = new Date().toISOString().slice(0, 10);;
+var takealotFile = ('Takealot_' + date + '.json');
+
+var links = [
+    'http://www.takealot.com/deals',
+    'http://www.takealot.com/deals/app-only'
+];
+
+var productHash = {};
+var products = [];
+
+casper.start().each(links, function(self, link) {
+    self.thenOpen(link, function()
+    {
+        var listTitles = this.getElementsInfo('h3.deal-heading');
+        for (var i = 0; i < listTitles.length; i++)
+        {	
+            var productDetails = {};
+            productDetails['title'] = listTitles[i].text;
+            products.push(productDetails);
+        }
+        productHash['products'] = products;
+        fs.write(takealotFile, JSON.stringify(productHash), 'a');
+    });
+})
+
+casper.then(function() {
+    this.exit();
+});  
+
+casper.run(function() {
+});
