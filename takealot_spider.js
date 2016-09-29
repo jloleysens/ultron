38b15134739a544755aec0e0bcf5fa35c6784f05
Abraham de Villiers <jaco_ftw@iafrica.com>
2016-09-29 15:36:25 +0200

    Added Deal Prices to Array. Spider now pulls Product Names and Prices and saves in JSON File. Started experimenting with XPath to find solution for Product URL and IMG.

--- takealot_spider.js
+++ takealot_spider.js
@@ -6,6 +6,7 @@ var casper = require('casper').create(
 	}
 });
 
+//var x = require('casper').selectXPath;
 var fs = require('fs');
 var date = new Date().toISOString().slice(0, 10);;
 var takealotFile = ('Takealot_' + date + '.json');
@@ -22,10 +23,14 @@ casper.start().each(links, function(self, link) {
     self.thenOpen(link, function()
     {
         var listTitles = this.getElementsInfo('h3.deal-heading');
+        var listPrices = this.getElementsInfo('p.price');
+
         for (var i = 0; i < listTitles.length; i++)
         {	
             var productDetails = {};
             productDetails['title'] = listTitles[i].text;
+            productDetails['price'] = listPrices[i].text;
+            //productDetails['url'] = listUrl[i].text;
             products.push(productDetails);
         }
         productHash['products'] = products;
